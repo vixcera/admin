26e5dc20fe2a449c1f17f7c3998ccfcb5180f2d5
vixcera <vixceraa@gmail.com>
2024-01-30 20:48:04 +0700

    upload file admin

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -36,7 +36,7 @@ const Products = () => {
     return (
         <>
         {(status !== 200) && (
-            <div style={{marginTop: '40px'}}>
+            <div style={{marginTop: '70px'}}>
                 <Handle status={status}/>
             </div>
         )}
