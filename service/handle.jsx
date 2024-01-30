042223ab6b82702fec5328b89fcea8633d23c995
vixcera <vixceraa@gmail.com>
2024-01-30 20:49:37 +0700

    upload file admin

--- service/handle.jsx
+++ service/handle.jsx
@@ -3,12 +3,12 @@ const Handle = ({ status }) => {
         <div className="page-max" style={{flexDirection: 'column', gap: '30px'}}>
             {(status == 404) ? (
                 <>
-                <img src="/img/404page.png" style={{width: '250px'}}/>
+                <img src="/img/404.png" style={{width: '250px'}}/>
                 <div className="title" style={{fontSize: '1.4rem'}}>Products not found</div>
                 </>
             ) : (
                 <>
-                <img src="/img/200page.png" style={{width: '250px'}}/>
+                <img src="/img/404.png" style={{width: '250px'}}/>
                 <div className="title" style={{fontSize: '1.4rem'}}>Server maintenance</div>
                 </>
             )}
