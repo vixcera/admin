56546ade776b099de18e838a03341f9dfb18a992
vixcera <vixceraa@gmail.com>
2024-01-30 21:15:40 +0700

    upload file admin

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -39,8 +39,8 @@ const Content = ({data, setData, setCount}) => {
                     </div>
                 }
             </div>
-            {(path == '/') && <Handle status={404}/>}
-            {(path == '/about') && <Handle status={404}/>}
+            {(path == '/') && <div style={{marginTop: '80px'}}><Handle status={404}/></div>}
+            {(path == '/about') && <div style={{marginTop: '80px'}}><Handle status={404}/></div>}
             {(path == '/products') && <Products/>}
             
         </div>
