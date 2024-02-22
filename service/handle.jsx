7aae711931a98241b2ad12057e286e0e5fd2cbcb
vixcera <vixceraa@gmail.com>
2024-02-22 14:51:16 +0700

    upload file admin

--- service/handle.jsx
+++ service/handle.jsx
@@ -4,10 +4,12 @@ const Handle = ({ status }) => {
             {(status == 404) ? (
                 <>
                 <img src="/img/404.png" style={{width: '250px'}}/>
+                <div className="title" style={{fontSize: '1.4rem'}}>Unexpected error</div>
                 </>
             ) : (
                 <>
                 <img src="/img/404.png" style={{width: '250px'}}/>
+                <div className="title" style={{fontSize: '1.4rem'}}>Unexpected error</div>
                 </>
             )}
         </div>
